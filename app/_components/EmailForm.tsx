809b47a08ff43ec1f2912683722c0f8a4e7ff2be
Smith11b <wariyor1@gmail.com>
2024-12-11 10:49:35 -0600

    added functionality for input to give positive feedback

--- app/_components/EmailForm.tsx
+++ app/_components/EmailForm.tsx
@@ -4,6 +4,7 @@ import styles from "../page.module.css";
 
 export default function EmailForm() {
   const [email, setEmail] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -12,36 +13,41 @@ export default function EmailForm() {
   const handleLinkClick = async (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     event.preventDefault();
     try {
-        const response = await fetch('/api/email', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email }),
-        });
+      const response = await fetch('/api/email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
 
-        if (!response.ok) {
-          throw new Error('Failed to submit email');
-        }
-
-        const data = await response.json();
-        console.log('Email submitted successfully:', data);
-      } catch (error) {
-        console.error('Error submitting email:', error);
+      if (!response.ok) {
+        throw new Error('Failed to submit email');
       }
-    };
+
+      const data = await response.json();
+      console.log('Email submitted successfully:', data);
+      setSubmitted(true); // Change the state to submitted
+    } catch (error) {
+      console.error('Error submitting email:', error);
+    }
+  };
 
   return (
-<div className={styles.cta}>
-      <a href="/signup" className={styles.button} onClick={handleLinkClick}>Join the waitlist</a>
-      <input
-        type="email"
-        placeholder="Enter your email"
-        className={styles.emailInput}
-        value={email}
-        onChange={handleEmailChange}
-        required
-      />
-      </div>
+    <div className={`${styles.cta} ${submitted ? styles.submitted : ''}`}>
+      <a href="/signup" className={styles.button} onClick={handleLinkClick}>
+        {submitted ? 'Thanks for joining us!' : 'Join the waitlist'}
+      </a>
+      {!submitted && (
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className={styles.emailInput}
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      )}
+    </div>
   );
 }
