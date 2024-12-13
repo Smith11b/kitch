5d03407f9fd964a3fb4b43e16aef0d71b1b21d35
Smith11b <wariyor1@gmail.com>
2024-12-13 10:07:57 -0600

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
       <div className="root-container">
         {children}
+        <Analytics />
         </div>
       </body>
     </html>
